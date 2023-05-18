e9b4c599951429682be99035130db33f1c95cdf7
Bekalu Geta <bekalu2054@gmail.com>
2023-05-18 14:00:03 +0300

    [feat]: Add login and register views

--- enen/core/views.py
+++ enen/core/views.py
@@ -1,4 +1,288 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Doctor, Patient, Prescription, passwordHasher, emailHasher
+from django.db.models import Count, Q
 
 def index(request):
-    return render(request, 'core/index.html')
+    """ Function for displaying main page of website. """
+    # Editing response headers so as to ignore cached versions of pages
+    response = render(request,"core/index.html")
+    return responseHeadersModifier(response)
+
+def register(request):
+    """ Function for displaying registration page. """
+
+    # If user wants to register
+    if request.method == "GET":
+        # Editing response headers so as to ignore cached versions of pages
+        response = render(request,"core/register.html")
+        return responseHeadersModifier(response)
+    
+    # If user wants to submit registration form
+    elif request.method == "POST":
+
+        # Getting data from form
+        userFirstName = request.POST["userFirstName"]
+        userLastName = request.POST["userLastName"]
+        userEmail = request.POST["userEmail"]
+        userRollNo = request.POST["userRollNo"]
+        userAddress = request.POST["userAddress"]
+        userContactNo = request.POST["userContactNo"]
+        userPassword = request.POST["userPassword"]
+        userConfirmPassword = request.POST["userConfirmPassword"]
+
+        # If both the passwords match
+        if userPassword == userConfirmPassword:
+
+            name = userFirstName + " " + userLastName
+
+            # Encrypting password to store inside database
+            passwordHash = passwordHasher(userPassword)
+
+            # Encrypting email to store inside database
+            emailHash = emailHasher(userEmail)
+
+            # Creating a patient object and saving inside the database
+            patient = Patient(name = name,rollNumber = userRollNo, email = userEmail, passwordHash = passwordHash, address = userAddress, contactNumber = userContactNo, emailHash = emailHash )
+            patient.save()
+
+            # Storing success message in the context variable
+            context = {
+                "message":"User Registration Successful. Please Login."
+            }
+
+            # Editing response headers so as to ignore cached versions of pages
+            response = render(request, "core/register.html",context)
+            return responseHeadersModifier(response)
+
+        # If the passwords given don't match
+        else:
+            # Storing failure message in the context variable
+            context = {
+                "message":"Passwords do not match. Please register again."
+            }
+
+            # Editing response headers so as to ignore cached versions of pages
+            response = render(request,"core/register.html",context)
+            return responseHeadersModifier(response)
+
+    # For any other method of request, sending back the registration page.
+    else:
+
+        # Editing response headers so as to ignore cached versions of pages
+        response = render(request,"core/register.html.html")
+        return responseHeadersModifier(response)
+
+def doctors(request):
+    """Function to send information about the doctors available to the user. """
+
+    # Storing doctors available in the context variable
+    context = {
+        "doctors" : Doctor.objects.all()
+    }
+
+    # Editing response headers so as to ignore cached versions of pages
+    response = render(request,"core/doctors.html",context)
+    return responseHeadersModifier(response)
+
+def login(request):
+    """ Function for logging in the user. """
+
+    # Calling session variables checker
+    request = requestSessionInitializedChecker(request)
+
+    # If the request method is post
+    if request.method == "GET":
+        try:
+
+            # If the user is already logged in inside of his sessions, and is a doctor, then no authentication required
+            if request.session['isLoggedIn'] and request.session['isDoctor']:
+
+                # Accessing the doctor user and all his/her records
+                doctor = Doctor.objects.get(emailHash = request.session['userEmail'])
+                records = doctor.doctorRecords.all()
+
+                # Getting the count of the new prescriptions pending
+                numberNewPendingPrescriptions = doctor.doctorRecords.aggregate(newPendingPrescriptions = Count('pk', filter =( Q(isNew = True) & Q(isCompleted = False) ) ))['newPendingPrescriptions']
+
+                # Storing the same inside the session variables
+                request.session['numberNewPrescriptions'] = numberNewPendingPrescriptions
+
+                # Storing the required information inside the context variable
+                context = {
+                    "message" : "Successfully Logged In.",
+                    "isAuthenticated" : True,
+                    "user": records.order_by('-timestamp')
+                }
+
+                # Editing response headers so as to ignore cached versions of pages
+                response = render(request,"core/userDoctorProfilePortal.html", context)
+                return responseHeadersModifier(response)
+
+            # If the user is already logged in inside of his sessions, and is a patient, then no authentication required
+            elif request.session['isLoggedIn'] and (not request.session['isDoctor']):
+
+                # Accessing the patient user and all his/her records
+                patient = Patient.objects.get(emailHash = request.session['userEmail'])
+                records = patient.patientRecords.all()
+
+                # Getting the count of the new prescriptions pending
+                numberNewPrescriptions = patient.patientRecords.aggregate(newCompletedPrescriptions = Count('pk', filter =( Q(isNew = True) & Q(isCompleted = True) ) ) )['newCompletedPrescriptions']
+
+                # Storing the same inside the session variables
+                request.session['numberNewPrescriptions'] = numberNewPrescriptions
+
+                # Updating the completed records
+                for record in records:
+                    if record.isCompleted:
+                        record.isNew = False
+                        record.save()
+
+                # Storing the required information inside the context variable
+                context = {
+                    "message" : "Successfully Logged In.",
+                    "isAuthenticated" : True,
+                    "user": records.order_by('-timestamp')
+                    }
+
+                # Editing response headers so as to ignore cached versions of pages
+                response = render(request,"core/userPatientProfilePortal.html", context)
+                return responseHeadersModifier(response)
+
+            else:
+                # Editing response headers so as to ignore cached versions of pages
+                response = render(request,"core/login.html")
+                return responseHeadersModifier(response)
+
+        # If any error occurs, sending back a new blank page
+        except:
+
+            # Editing response headers so as to ignore cached versions of pages
+            response = render(request,"core/login.html")
+            return responseHeadersModifier(response)
+
+    # If the request method is post
+    elif request.method == "POST":
+
+        # Extracting the user information from the post request
+        userName = request.POST["useremail"]
+        userPassword = request.POST["userpassword"]
+
+        # If such a patient exists
+        try:
+            patient = Patient.objects.get(email = userName)
+
+            # Storing required session information
+            request.session['isDoctor'] = False
+
+        # Otherwise trying if a doctor exists
+        except Patient.DoesNotExist:
+            try:
+                doctor = Doctor.objects.get(email = userName)
+
+                # Storing required session information
+                request.session['isDoctor'] = True
+
+            # If no such doctor or patient exists
+            except Doctor.DoesNotExist:
+
+                # Storing message inside context variable
+                context = {
+                    "message":"User does not exist.Please register first."
+                }
+
+                # Editing response headers so as to ignore cached versions of pages
+                response = render(request,"core/login.html", context)
+                return responseHeadersModifier(response)
+
+        # Getting the hash of user inputted password
+        passwordHash = passwordHasher(userPassword)
+
+        # If the logged in user is a doctor
+        if request.session['isDoctor']:
+
+            # Accessing all records of doctor
+            records = doctor.doctorRecords.all()
+
+            # Getting the count of new prescriptions
+            numberNewPendingPrescriptions = doctor.doctorRecords.aggregate(newPendingPrescriptions = Count('pk', filter =( Q(isNew = True) & Q(isCompleted = False) ) ))['newPendingPrescriptions']
+
+            # Storing the same inside request variable
+            request.session['numberNewPrescriptions'] = numberNewPendingPrescriptions
+
+            # If the inputted hash and the original user password hash match
+            if passwordHash == doctor.passwordHash:
+
+                # Storing required information in session variable of request
+                request.session['isLoggedIn'] = True
+                request.session['userEmail'] = doctor.emailHash
+                request.session['Name'] = doctor.name
+
+                # Redirecting to avoid form resubmission
+                # Redirecting to home page
+                # Editing response headers so as to ignore cached versions of pages
+                response = HttpResponseRedirect(reverse('index'))
+                return responseHeadersModifier(response)
+
+            # Else if the password inputted is worng and doesn't match
+            else:
+
+                # Storing message inside context variable
+                context = {
+                    "message":"Invalid Credentials.Please Try Again."
+                }
+
+                # Editing response headers so as to ignore cached versions of pages
+                response = render(request,"core/login.html", context)
+                return responseHeadersModifier(response)
+
+        # Otherwise if the user is a patient
+        else:
+
+            # Accessing all records of patient
+            records = patient.patientRecords.all()
+
+            # Getting the count of new prescriptions
+            numberNewPrescriptions = patient.patientRecords.aggregate(newCompletedPrescriptions = Count('pk', filter =( Q(isNew = True) & Q(isCompleted = True) ) ))['newCompletedPrescriptions']
+
+            # Storing the same inside request variable
+            request.session['numberNewPrescriptions'] = numberNewPrescriptions
+
+            # Updating the completed records
+            for record in records:
+                if record.isCompleted :
+                    record.isNew = False
+                    record.save()
+
+            # If the inputted hash and the original user password hash match
+            if passwordHash == patient.passwordHash:
+
+                # Storing required information in session variable of request
+                request.session['isLoggedIn'] = True
+                request.session['userEmail'] = patient.emailHash
+                request.session['Name'] = patient.name
+                request.session['isDoctor'] = False
+
+                # Redirecting to avoid form resubmission
+                # Redirecting to home page
+                # Editing response headers so as to ignore cached versions of pages
+                response = HttpResponseRedirect(reverse('index'))
+                return responseHeadersModifier(response)
+
+            # Else if the password inputted is wrong and doesn't match
+            else:
+
+                # Storing message inside context variable
+                context = {
+                    "message":"Invalid Credentials. Please Try Again."
+                }
+
+                # Editing response headers so as to ignore cached versions of pages
+                response = render(request,"core/login.html", context)
+                return responseHeadersModifier(response)
+    # For any other method of access, returning a new blank login page
+    else:
+        response = render(request,"core/login.html")
+        return responseHeadersModifier(response)
