8c2f309cc39ebf4442b902cd4a12dd847f5185c0
Bekalu Geta <bekalu2054@gmail.com>
2023-05-28 21:38:08 +0300

    [feat]: save displaying profile image

--- enen/enen/settings.py
+++ enen/enen/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-98u+^ibkz@u#mhpn7t9u9(d^xs2rheq2-pqzjd)y+5wd$%0&s%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['enen.bekalue.tech', 'bekalue.tech', 'localhost', '127.0.0.1']
 
